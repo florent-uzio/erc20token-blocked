36bfbaa5dbfb1147f5a792b1aa63b0428499aaf3
Florent Uzio <fuzio@ripple.com>
2024-02-01 15:49:57 +0400

    init contract

---
+++ contracts/contracts/LapinuToken.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is ERC20, ERC20Pausable, Ownable {
+    constructor(
+        address initialOwner
+    ) ERC20("MyToken", "MTK") Ownable(initialOwner) {
+        _mint(msg.sender, 10000000 * 10 ** decimals());
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(
+        address from,
+        address to,
+        uint256 value
+    ) internal override(ERC20, ERC20Pausable) {
+        super._update(from, to, value);
+    }
+}
