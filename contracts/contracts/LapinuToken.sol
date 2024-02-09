8792c40a48a7f1c5020559b4e30b85fb197296e2
Florent Uzio <fuzio@ripple.com>
2024-02-09 16:56:27 +0400

    import other contract

--- contracts/contracts/LapinuToken.sol
+++ contracts/contracts/LapinuToken.sol
@@ -5,6 +5,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+import "./BlockListLapinu.sol";
+
 contract MyToken is ERC20, ERC20Pausable, Ownable {
     constructor(
         address initialOwner
